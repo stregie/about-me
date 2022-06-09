145b409e1eb768c0897613a63d71444000e5f2d5
stregie <stregie@outlook.com>
2022-06-09 13:28:23 +0200

    Header-footer frame restructured, file uploader completed

---
+++ handlers/handlers01.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const formidable = require('formidable');
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl = "mongodb+srv://st:gSaCRgRWC4niM4fd@introduction-site.8s83u.mongodb.net/?retryWrites=true&w=majority";
+
+exports.main = (req, res) => {
+  res.render('01-main.ejs', null);
+};
+
+exports.snake = (req, res) => {
+  res.render('02-snake.ejs', null);
+};
+
+exports.mongotest = (req, res) => {
+  let mongodat = {};
+  mongodat.data = "This is hardcoded in app.js. If everything goes fine, something from a mongodb should be here";
+
+  MongoClient.connect(mongoUrl, { useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    let dbo = db.db("introduction-site");
+  
+    dbo.collection("test").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      mongodat.data = result[0].message;
+      db.close();
+      res.render('03-mongotest.ejs', mongodat);
+    });
+  });
+};
+
+exports.fileuploader = (req, res) => {
+	res.render('04-fileuploader.ejs', null);
+};
+
+exports.fileuploaderGetUploadedFiles = (req, res) => {
+  let dirpath = path.join(__dirname, '..', 'uploaded_files');
+  let filelist = [];
+  fs.readdir(dirpath, (err, files) => {
+    files.forEach((file, index) => {
+      filelist[index] = {filename: file};
+      let stats = fs.statSync(dirpath + "/" + file);
+      filelist[index].filesize = stats.size;
+    });
+    res.json(filelist);
+  });
+};
+
+exports.fileuploaderPostFiles = (req, res) => {
+  let dirpath = path.join(__dirname, '..', 'uploaded_files');
+  
+  const form = new formidable.IncomingForm();
+  form.multiples = true;
+  form.maxFileSize = 50 * 1024 * 1024; // FieldsSize? sth doesnt work now.
+  form.uploadDir = dirpath;
+
+  form.parse(req);
+
+  form.on('fileBegin', function (name, file) { // name: form name; file: file object. Called when file uploaded but not saved 
+    file.filepath = path.join(dirpath, file.originalFilename);
+  });
+
+  form.on('file', function (name, file) { // called when file uploaded and saved.   
+    console.log('Uploaded ' + file.originalFilename);      
+  });
+
+  res.send("Files successfully uploaded!");
+};
+
+exports.fileuploaderDownload = (req, res) => {
+  let dirpath = path.join(__dirname, '..', 'uploaded_files');
+  console.log(req.query.file);
+  let filepath = dirpath + "\\" + req.query.file;
+  res.download(filepath);
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,64 @@
 const express = require ('express');
 const router = express.Router();
+
 const ejs = require ('ejs');
 
-// const controller01 = require('../controllers/controller01.js');
 
-router.get('/', (req, res) => {
-  let pagedata = {};
-  ejs.renderFile('./views/01-main.ejs', (err, str) => {
-    pagedata.content = str;
-    res.render('00-frame.ejs', pagedata);
-  });
-});
+const handlers01 = require('../handlers/handlers01.js');
 
-router.get('/snake', (req, res) => {
-  renderPage(req, res, '02-snake.ejs');
-});
+router.get('/', handlers01.main);
+router.get('/snake', handlers01.snake);
+router.get('/mongotest', handlers01.mongotest);
+router.get('/fileuploader', handlers01.fileuploader);
+router.get('/fileuploader/getuploadedfiles', handlers01.fileuploaderGetUploadedFiles);
+router.post('/fileuploader/uploadfiles', handlers01.fileuploaderPostFiles);
+router.get('/fileuploader/download/', handlers01.fileuploaderDownload);
 
-// router.get('/01a/', controller01.ctrl01a);
-// router.post('/01b/sendmessage', controller01.ctrl01b);
 
-router.get('/c1', (req, res) => {
-  renderPage(req, res, '01-main.ejs');
-});
 
-function renderPage(req, res, ejsName) {
-  let pagedata = {};
-  let path = './views/' + ejsName;
-  ejs.renderFile(path, (err, str) => {
-    pagedata.content = removeHtmlTag(str);
-    res.render('00-frame.ejs', pagedata);
-  });
-};
 
-function removeHtmlTag(html){
-  let htmlTagPos = html.indexOf("<html>");
-  let perHtmlTagPos = html.indexOf("</html>");
-  if (htmlTagPos >= 0 && perHtmlTagPos >= 0){
-    return html.slice(htmlTagPos + 7, perHtmlTagPos - 1);
-  } else {
-    return html;
-  }
-};
+// router.get('/', (req, res) => {
+//   let pagedata = {};
+//   ejs.renderFile('./views/01-main.ejs', (err, str) => {
+//     pagedata.content = str;
+//     res.render('00-frame.ejs', pagedata);
+//   });
+// });
+
+// router.get('/snake', (req, res) => {
+//   renderPage(req, res, '02-snake.ejs');
+// });
+
+// // router.get('/01a/', controller01.ctrl01a);
+// // router.post('/01b/sendmessage', controller01.ctrl01b);
+
+// router.get('/c1', (req, res) => {
+//   let pagedatac1 = {};
+//   // pagedatac1.data = "kukuriku";
+//   pagedatac1.dataxy = null;
+//   renderPage(req, res, '01-main.ejs', pagedatac1);
+// });
+
+
+
+// function renderPage(req, res, ejsName, datae) {
+//   let pagedata = {};
+//   console.log(datae);
+//   let path = './views/' + ejsName;
+//   ejs.renderFile(path, datae, (err, str) => {
+//     pagedata.content = removeHtmlTag(str);
+//     res.render('00-frame.ejs', pagedata);
+//   });
+// };
+
+// function removeHtmlTag(html){
+//   let htmlTagPos = html.indexOf("<html>");
+//   let perHtmlTagPos = html.indexOf("</html>");
+//   if (htmlTagPos >= 0 && perHtmlTagPos >= 0){
+//     return html.slice(htmlTagPos + 7, perHtmlTagPos - 1);
+//   } else {
+//     return html;
+//   }
+// };
 
 module.exports = router;
