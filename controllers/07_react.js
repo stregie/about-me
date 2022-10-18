c5cff1c6b96902650e6c31ff70594e259f049a36
stregie <stregie@outlook.com>
2022-10-18 14:01:10 +0200

    React Gallery first version

--- controllers/07_react.js
+++ controllers/07_react.js
@@ -1,6 +1,54 @@
-// const path = require('path');
+const path = require('path');
+const fs = require('fs');
 
 exports.main = (req, res) => {
-  // res.send("OK");
   res.render('07_react.ejs', null);
+};
+
+exports.fileList = (req, res) => {
+  let album = req.query.album;
+  let dirpath = path.join(__dirname, '..', '/public/images/', album);
+  
+
+  let filelist = [];
+  fs.readdir(dirpath, (err, files) => {
+    const images = files.filter(file => {
+      let ext = path.extname(file).toLowerCase()
+      return ext === '.jpg' || ext === '.jpeg' || ext === '.png' || ext === '.webp';
+    });
+    images.forEach((imgfile, index) => {
+      filelist[index] = {filename: imgfile};
+      let stats = fs.statSync(dirpath + "/" + imgfile);
+      filelist[index].filesize = stats.size;
+    });
+    res.json(filelist);
+  });
+};
+
+exports.sendImage = (req, res) => {
+  let album = req.params.album;
+  let imgname = req.params.image;
+  // console.log(process.env.PUBLIC_URL)
+
+  let sendFileOptions = {
+    root: path.join(__dirname, '..', '/public/images/', album)
+  };
+  
+  let imgpath = path.join(sendFileOptions.root, imgname);
+  let imgExist = fs.existsSync(imgpath);
+  if (imgExist) {
+      res.sendFile(imgname, sendFileOptions, (err) => {
+        if (err) {
+          res.status(418)
+          console.log("sendImage failed", err);
+          res.end();
+        } else {
+          // console.log("Image sent successfully");
+          res.end();
+        }
+      });
+  } else {
+    res.status(404)
+    res.end(imgname, " not found.");
+  }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-	const express = require ('express');
+const express = require ('express');
 const router = express.Router();
 
 const ejs = require ('ejs');
@@ -8,7 +8,7 @@ const controllers = require('../controllers/01_general.js');
 const snake = require('../controllers/04_snake.js');
 const news = require('../controllers/05_news.js');
 const fileuploader = require('../controllers/06_fileuploader.js');
-// const react = require('../controllers/07_react.js');
+const react = require('../controllers/07_react.js');
 
 router.get('/', controllers.main);
 router.get('/aboutme/', controllers.aboutme);
@@ -34,7 +34,10 @@ router.get('/fileuploader/getuploadedfiles', fileuploader.fileuploaderGetUploade
 router.post('/fileuploader/uploadfiles', fileuploader.fileuploaderPostFiles);
 router.get('/fileuploader/download/', fileuploader.fileuploaderDownload);
 
-// router.get('/react/', react.main);
+router.get('/react/*', react.main);
+// router.get('/react/*', react.main);
+router.get('/reactapi/getfilelist/', react.fileList);
+router.get('/reactapi/images/:album/:image', react.sendImage);
 
 router.get('/mongotest/', controllers.mongotest);
 
